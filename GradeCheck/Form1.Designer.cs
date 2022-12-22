eb01a67d976eb27f4ff10d0fd6ec4db81715eaa3
okayu <yuto.gio.0515@gamil.com>
2022-12-23 00:04:24 +0900

    プロジェクト ファイルを追加します。

---
+++ GradeCheck/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿
+namespace GradeCheck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtMathAttend = new System.Windows.Forms.TextBox();
+            this.txtPhysicsAttend = new System.Windows.Forms.TextBox();
+            this.txtEngAttend = new System.Windows.Forms.TextBox();
+            this.txtMathPoint = new System.Windows.Forms.TextBox();
+            this.txtPhysicsPoint = new System.Windows.Forms.TextBox();
+            this.txtEngPoint = new System.Windows.Forms.TextBox();
+            this.lblMathJudge = new System.Windows.Forms.Label();
+            this.lblPhysicsJudge = new System.Windows.Forms.Label();
+            this.lblEngJudge = new System.Windows.Forms.Label();
+            this.lblMathCompare = new System.Windows.Forms.Label();
+            this.lblPhysicsCompare = new System.Windows.Forms.Label();
+            this.lblEngCompare = new System.Windows.Forms.Label();
+            this.btnJudge = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "科目";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "数学";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 12);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "物理";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "英語";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(120, 32);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 12);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "出席率";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(280, 32);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "得点";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(422, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "判定結果";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(571, 32);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 12);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "平均点と比較";
+            // 
+            // txtMathAttend
+            // 
+            this.txtMathAttend.Location = new System.Drawing.Point(122, 60);
+            this.txtMathAttend.Name = "txtMathAttend";
+            this.txtMathAttend.Size = new System.Drawing.Size(100, 19);
+            this.txtMathAttend.TabIndex = 0;
+            // 
+            // txtPhysicsAttend
+            // 
+            this.txtPhysicsAttend.Location = new System.Drawing.Point(122, 100);
+            this.txtPhysicsAttend.Name = "txtPhysicsAttend";
+            this.txtPhysicsAttend.Size = new System.Drawing.Size(100, 19);
+            this.txtPhysicsAttend.TabIndex = 1;
+            // 
+            // txtEngAttend
+            // 
+            this.txtEngAttend.Location = new System.Drawing.Point(122, 140);
+            this.txtEngAttend.Name = "txtEngAttend";
+            this.txtEngAttend.Size = new System.Drawing.Size(100, 19);
+            this.txtEngAttend.TabIndex = 2;
+            // 
+            // txtMathPoint
+            // 
+            this.txtMathPoint.Location = new System.Drawing.Point(282, 57);
+            this.txtMathPoint.Name = "txtMathPoint";
+            this.txtMathPoint.Size = new System.Drawing.Size(100, 19);
+            this.txtMathPoint.TabIndex = 3;
+            // 
+            // txtPhysicsPoint
+            // 
+            this.txtPhysicsPoint.Location = new System.Drawing.Point(282, 97);
+            this.txtPhysicsPoint.Name = "txtPhysicsPoint";
+            this.txtPhysicsPoint.Size = new System.Drawing.Size(100, 19);
+            this.txtPhysicsPoint.TabIndex = 4;
+            // 
+            // txtEngPoint
+            // 
+            this.txtEngPoint.Location = new System.Drawing.Point(282, 137);
+            this.txtEngPoint.Name = "txtEngPoint";
+            this.txtEngPoint.Size = new System.Drawing.Size(100, 19);
+            this.txtEngPoint.TabIndex = 5;
+            // 
+            // lblMathJudge
+            // 
+            this.lblMathJudge.AutoSize = true;
+            this.lblMathJudge.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblMathJudge.Location = new System.Drawing.Point(422, 60);
+            this.lblMathJudge.Name = "lblMathJudge";
+            this.lblMathJudge.Size = new System.Drawing.Size(61, 19);
+            this.lblMathJudge.TabIndex = 6;
+            this.lblMathJudge.Text = "label1";
+            // 
+            // lblPhysicsJudge
+            // 
+            this.lblPhysicsJudge.AutoSize = true;
+            this.lblPhysicsJudge.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblPhysicsJudge.Location = new System.Drawing.Point(422, 100);
+            this.lblPhysicsJudge.Name = "lblPhysicsJudge";
+            this.lblPhysicsJudge.Size = new System.Drawing.Size(61, 19);
+            this.lblPhysicsJudge.TabIndex = 7;
+            this.lblPhysicsJudge.Text = "label1";
+            // 
+            // lblEngJudge
+            // 
+            this.lblEngJudge.AutoSize = true;
+            this.lblEngJudge.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblEngJudge.Location = new System.Drawing.Point(422, 140);
+            this.lblEngJudge.Name = "lblEngJudge";
+            this.lblEngJudge.Size = new System.Drawing.Size(61, 19);
+            this.lblEngJudge.TabIndex = 8;
+            this.lblEngJudge.Text = "label1";
+            // 
+            // lblMathCompare
+            // 
+            this.lblMathCompare.AutoSize = true;
+            this.lblMathCompare.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblMathCompare.Location = new System.Drawing.Point(571, 60);
+            this.lblMathCompare.Name = "lblMathCompare";
+            this.lblMathCompare.Size = new System.Drawing.Size(61, 19);
+            this.lblMathCompare.TabIndex = 9;
+            this.lblMathCompare.Text = "label1";
+            // 
+            // lblPhysicsCompare
+            // 
+            this.lblPhysicsCompare.AutoSize = true;
+            this.lblPhysicsCompare.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblPhysicsCompare.Location = new System.Drawing.Point(571, 100);
+            this.lblPhysicsCompare.Name = "lblPhysicsCompare";
+            this.lblPhysicsCompare.Size = new System.Drawing.Size(61, 19);
+            this.lblPhysicsCompare.TabIndex = 10;
+            this.lblPhysicsCompare.Text = "label1";
+            // 
+            // lblEngCompare
+            // 
+            this.lblEngCompare.AutoSize = true;
+            this.lblEngCompare.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.lblEngCompare.Location = new System.Drawing.Point(571, 140);
+            this.lblEngCompare.Name = "lblEngCompare";
+            this.lblEngCompare.Size = new System.Drawing.Size(61, 19);
+            this.lblEngCompare.TabIndex = 11;
+            this.lblEngCompare.Text = "label1";
+            // 
+            // btnJudge
+            // 
+            this.btnJudge.Location = new System.Drawing.Point(260, 240);
+            this.btnJudge.Name = "btnJudge";
+            this.btnJudge.Size = new System.Drawing.Size(75, 23);
+            this.btnJudge.TabIndex = 12;
+            this.btnJudge.Text = "判定";
+            this.btnJudge.UseVisualStyleBackColor = true;
+            this.btnJudge.Click += new System.EventHandler(this.btnJudge_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(358, 240);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 13;
+            this.btnReset.Text = "リセット";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(228, 60);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(11, 12);
+            this.label15.TabIndex = 0;
+            this.label15.Text = "%";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(228, 103);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(11, 12);
+            this.label16.TabIndex = 0;
+            this.label16.Text = "%";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Location = new System.Drawing.Point(228, 143);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(11, 12);
+            this.label17.TabIndex = 0;
+            this.label17.Text = "%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 309);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnJudge);
+            this.Controls.Add(this.txtEngAttend);
+            this.Controls.Add(this.txtPhysicsAttend);
+            this.Controls.Add(this.txtEngPoint);
+            this.Controls.Add(this.txtPhysicsPoint);
+            this.Controls.Add(this.txtMathPoint);
+            this.Controls.Add(this.txtMathAttend);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblEngCompare);
+            this.Controls.Add(this.lblEngJudge);
+            this.Controls.Add(this.lblPhysicsCompare);
+            this.Controls.Add(this.lblPhysicsJudge);
+            this.Controls.Add(this.lblMathCompare);
+            this.Controls.Add(this.lblMathJudge);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "成績判定";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtMathAttend;
+        private System.Windows.Forms.TextBox txtPhysicsAttend;
+        private System.Windows.Forms.TextBox txtEngAttend;
+        private System.Windows.Forms.TextBox txtMathPoint;
+        private System.Windows.Forms.TextBox txtPhysicsPoint;
+        private System.Windows.Forms.TextBox txtEngPoint;
+        private System.Windows.Forms.Label lblMathJudge;
+        private System.Windows.Forms.Label lblPhysicsJudge;
+        private System.Windows.Forms.Label lblEngJudge;
+        private System.Windows.Forms.Label lblMathCompare;
+        private System.Windows.Forms.Label lblPhysicsCompare;
+        private System.Windows.Forms.Label lblEngCompare;
+        private System.Windows.Forms.Button btnJudge;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label17;
+    }
+}
+
---
+++ GradeCheck/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradeCheck
+{
+    public partial class Form1 : Form
+    {
+        private struct Attends
+        {
+            public double dblMathAttend;
+            public double dblPhysicsAttend;
+            public double dblEngAttend;
+        }
+        private struct Points
+        {
+            public int intMathPoint;
+            public int intPhysicsPoint;
+            public int intEngPoint;
+        }
+
+        enum Subjects
+        {
+            math,
+            Physics,
+            Eng
+        }
+
+        const int cstMathAve = 73;
+        const int cstPhysicsAve = 65;
+        const int cstEngAve = 77;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnReset_Click(sender, e);
+        }
+
+        private void btnJudge_Click(object sender, EventArgs e)
+        {
+            Attends attends = new Attends();
+            convTextToValue(txtMathAttend.Text, out attends.dblMathAttend);        //数学の出席率取得
+            convTextToValue(txtPhysicsAttend.Text, out attends.dblPhysicsAttend);  //物理の出席率取得
+            convTextToValue(txtEngAttend.Text, out attends.dblEngAttend);          //英語の出席率取得
+
+            Points points = new Points();
+            convTextToValue(txtMathPoint.Text, out points.intMathPoint);        //数学の点数取得
+            convTextToValue(txtPhysicsPoint.Text, out points.intPhysicsPoint);  //物理の点数取得
+            convTextToValue(txtEngPoint.Text, out points.intEngPoint);          //英語の点数取得
+
+            //成績判定
+            lblMathJudge.Text = judgeGrade(attends.dblMathAttend, points.intMathPoint);
+            lblPhysicsJudge.Text = judgeGrade(attends.dblPhysicsAttend, points.intPhysicsPoint);
+            lblEngJudge.Text = judgeGrade(attends.dblEngAttend, points.intEngPoint);
+
+            //平均点比較
+            lblMathCompare.Text = compareAverage(points.intMathPoint, Subjects.math);
+            lblPhysicsCompare.Text = compareAverage(points.intPhysicsPoint, Subjects.Physics);
+            lblEngCompare.Text = compareAverage(points.intEngPoint, Subjects.Eng);
+        }
+        private void convTextToValue(string input, out double value)
+        {
+            if (double.TryParse(input, out value) == false){
+                value = -1.0;
+            }
+        }
+        private void convTextToValue(string input, out int value)
+        {
+            if (int.TryParse(input, out value) == false)
+            {
+                value = -1;
+            }
+        }
+        private string judgeGrade(double attend, int point)
+        {
+            if (80 <= attend && attend <= 100)
+            {
+                if (80 <= point && point <= 100)
+                {
+                    return "A判定";
+                }
+                else if (70 <= point && point < 80)
+                {
+                    return "B判定";
+                }
+                else if (60 <= point && point < 70)
+                {
+                    return "C判定";
+                }
+                else if (0 <= point && point < 60)
+                {
+                    return "不合格";
+                }
+                else
+                {
+                    return "エラー";
+                }
+
+            }
+            else if (0 <= attend && attend < 80)
+            {
+                return "不合格";
+            }
+            else
+            {
+                return "エラー";
+            }
+        }
+        private string compareAverage(int point, Subjects subject)
+        {
+            switch (subject)
+            {
+                case Subjects.math:
+                    if (cstMathAve <= point)
+                    {
+                        return "平均点以上";
+                    }
+                    else
+                    {
+                        return "平均点以下";
+                    }
+
+                case Subjects.Physics:
+                    if (cstPhysicsAve <= point)
+                    {
+                        return "平均点以上";
+                    }
+                    else
+                    {
+                        return "平均点以下";
+                    }
+
+                case Subjects.Eng:
+                    if (cstEngAve <= point)
+                    {
+                        return "平均点以上";
+                    }
+                    else
+                    {
+                        return "平均点以下";
+                    }
+
+                default:
+                    return "エラー";
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            txtMathAttend.Text = "";
+            txtMathPoint.Text = "";
+            lblMathJudge.Text = "";
+            lblMathCompare.Text = "";
+            txtPhysicsAttend.Text = "";
+            txtPhysicsPoint.Text = "";
+            lblPhysicsJudge.Text = "";
+            lblPhysicsCompare.Text = "";
+            txtEngAttend.Text = "";
+            txtEngPoint.Text = "";
+            lblEngJudge.Text = "";
+            lblEngCompare.Text = "";
+        }
+    }
+}
